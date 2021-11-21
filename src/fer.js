5cb8475df9e0ab13483fe2b29b0cbb310ced244e
Yuliia-Lysiuk <84859026+Yuliia-Lysiuk@users.noreply.github.com>
2021-11-21 11:18:38 +0200

    do styles

---
+++ src/fer.js
@@ -0,0 +1,88 @@
+// import './css/styles.css';
+// import Notiflix from 'notiflix';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
+// const form = document.querySelector("#search-form");
+// const galleryConteiner = document.querySelector(".gallery");
+
+
+ 
+
+// form.addEventListener("submit", onSubmit);
+
+// let gallery = new SimpleLightbox('.gallery a', {
+//       showCounter: true,
+//       disableScroll: true,
+// });
+     
+// function onCardClick(evt) {
+//   evt.preventDefault();
+
+//   gallery.open('.gallery');
+// }
+
+// function onSubmit(e) {
+//     e.preventDefault();
+//     const inputValue = e.target.searchQuery.value;
+//     fetchPhoto(inputValue).then(renderPhoto).catch(error => {
+//         console.log(error);
+//     })
+    
+//     form.reset();
+// }
+
+ 
+
+// function fetchPhoto(inputValue) {
+//     return fetch(`https://pixabay.com/api/?key=24419358-338d9960aaa56c480bc3e3cda&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&webformatURL&largeImageURL&tags&likes&views&comments&downloads&per_page=40`).then(
+//         (response) => {
+//             if (!response.ok) {
+//                 throw new Error(response.status);
+//             }
+//             return response.json();
+//         }
+//     )    
+// }
+
+// function renderPhoto(photos) {
+//     console.log(photos.total);
+//     if (photos.total === 0) {
+//         galleryConteiner.innerHTML = "";
+//         return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again."); 
+//     }
+//     Notiflix.Notify.success(`Hooray! We found ${photos.totalHits} images.`);
+//     // galleryConteiner.addEventListener("click", onImageGallery);
+
+
+//     // gallery.addEventListener('click', onCardClick);
+//     // gallery.refresh();
+    
+//     const markup = photos.hits.map(({ tags, likes, webformatURL, comments, downloads, views, largeImageURL }) => {
+//         return `<a href="${largeImageURL}" class="photo-card">
+//             <img class="img" src="${webformatURL}" alt="${tags}" width="310" height="207" loading="lazy" />
+//             <div class="info">
+//                 <p class="info-item">
+//                     <b>Likes</b>
+//                     ${likes}
+//                 </p>
+//                 <p class="info-item">
+//                     <b>Views</b>
+//                     ${views}
+//                 </p>
+//                 <p class="info-item">
+//                     <b>Comments</b>
+//                     ${comments}
+//                 </p>
+//                 <p class="info-item">
+//                     <b>Downloads</b>
+//                     ${downloads}
+//                 </p>
+//             </div>
+//         </a>`
+//     }).join("");
+
+//     return galleryConteiner.innerHTML = markup;
+// }
--- src/index.js
+++ src/index.js
@@ -2,22 +2,42 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from "axios";
 
 
 
 const form = document.querySelector("#search-form");
-const gallery = document.querySelector(".gallery");
+const galleryConteiner = document.querySelector(".gallery");
 
-form.addEventListener("submit", (e) => {
+
+ 
+
+form.addEventListener("submit", onSubmit);
+
+// let gallery = new SimpleLightbox('.gallery a', {
+//       showCounter: true,
+//       disableScroll: true,
+// });
+     
+// function onCardClick(evt) {
+//   evt.preventDefault();
+
+//   gallery.open('.gallery');
+// }
+
+function onSubmit(e) {
     e.preventDefault();
     const inputValue = e.target.searchQuery.value;
     fetchPhoto(inputValue).then(renderPhoto).catch(error => {
         console.log(error);
     })
+    
     form.reset();
-})
+}
 
-async function fetchPhoto(inputValue) {
+ 
+
+function fetchPhoto(inputValue) {
     return fetch(`https://pixabay.com/api/?key=24419358-338d9960aaa56c480bc3e3cda&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&webformatURL&largeImageURL&tags&likes&views&comments&downloads&per_page=40`).then(
         (response) => {
             if (!response.ok) {
@@ -31,12 +51,13 @@ async function fetchPhoto(inputValue) {
 function renderPhoto(photos) {
     console.log(photos.total);
     if (photos.total === 0) {
-        gallery.innerHTML = "";
+        galleryConteiner.innerHTML = "";
         return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again."); 
     }
     Notiflix.Notify.success(`Hooray! We found ${photos.totalHits} images.`);
-    const markup = photos.hits.map(({ tags, likes, webformatURL, comments, downloads, views }) => {
-        return `<a><div class="photo-card">
+    
+    const markup = photos.hits.map(({ tags, likes, webformatURL, comments, downloads, views, largeImageURL }) => {
+        return `<a href="${largeImageURL}" class="photo-card">
             <img class="img" src="${webformatURL}" alt="${tags}" width="310" height="207" loading="lazy" />
             <div class="info">
                 <p class="info-item">
@@ -56,12 +77,8 @@ function renderPhoto(photos) {
                     ${downloads}
                 </p>
             </div>
-        </div></a>`
+        </a>`
     }).join("");
 
-    return gallery.innerHTML = markup;
+    return galleryConteiner.innerHTML = markup;
 }
-let gallerys = new SimpleLightbox('.gallery a', {
-  showCounter: true,
-  disableScroll: true,
-});

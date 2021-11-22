8fe893e6614245e8d60c6729a21c7768fb683a94
Yuliia-Lysiuk <84859026+Yuliia-Lysiuk@users.noreply.github.com>
2021-11-22 16:49:10 +0200

    зробила кнопку загрузки

--- src/index.js
+++ src/index.js
@@ -15,9 +15,7 @@ form.addEventListener("submit", onSubmit);
 
 let page = 1;
 
-let limit = 40;
-
-const totalPages = 500 / limit;
+let per_page = 40;
 
 let fetchItems;
 let gallery = new SimpleLightbox('.gallery a');
@@ -30,17 +28,17 @@ async function onSubmit(e) {
       return Notify.warning("Enter your search query, please!");
     }
     galleryConteiner.innerHTML = ''
-    page = 1
+  page = 1;
+  per_page = 40;
     fetchItems = async function() {
       try {
         const photo = await fetchPhoto(inputValue);
         renderPhoto(photo);
-          page += 1; 
-     
+        page += 1;
         loadMoreBtn.addEventListener("click", onLoadMore)
         gallery.refresh();
       } catch (error) {
-        console.dir(error);
+        console.dir(error); 
       }
     }
     fetchItems()
@@ -49,7 +47,7 @@ async function onSubmit(e) {
 
 async function fetchPhoto(inputValue) {
   try {
-    const response = await axios.get(`https://pixabay.com/api/?key=24419358-338d9960aaa56c480bc3e3cda&q=${inputValue}&page=${page}&image_type=photo&orientation=horizontal&safesearch=true&webformatURL&largeImageURL&tags&likes&views&comments&downloads&per_page=40`);
+    const response = await axios.get(`https://pixabay.com/api/?key=24419358-338d9960aaa56c480bc3e3cda&q=${inputValue}&page=${page}&image_type=photo&orientation=horizontal&safesearch=true&webformatURL&largeImageURL&tags&likes&views&comments&downloads&per_page=${per_page}`);
     return response.data
   } catch (e) {
     return Promise.reject(e);
@@ -57,26 +55,43 @@ async function fetchPhoto(inputValue) {
 }
 
 function renderPhoto(photos) {
+  console.log(photos);
+  if (photos.total === 0) {
+    clearGalleryConteiner();
+    hideBtnLoadMore();
+    return Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+  }
+  showBtnLoadMore();
+      Notify.success(`Hooray! We found ${photos.totalHits} images.`);
+      makeCard(photos);
+  if  (photos.total < 41 || photos.hits.length < 40) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    return hideBtnLoadMore();
+  }
+      showBtnLoadMore();
+      makeCard(photos);
+}
 
-    if (photos.total === 0) {
-        clearGalleryConteiner();
-        hideBtnLoadMore();
-        return Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-    } else if (photos.total < 41) {
+function clearGalleryConteiner() {
+    galleryConteiner.innerHTML = '';
+}
 
-        showBtnLoadMore();
-        loadMoreBtn.addEventListener("click", () => {
-            loadMoreBtn.getAttribute('disabled')
-            hideBtnLoadMore();
-            Notify.info("We're sorry, but you've reached the end of search results.")
-        }
-        );
-    } 
-    showBtnLoadMore();
-    
-        Notify.success(`Hooray! We found ${photos.totalHits} images.`);
-    
-    const markup = photos.hits.map(({ tags, likes, webformatURL, comments, downloads, views, largeImageURL }) => {
+function hideBtnLoadMore() {
+    loadMoreBtn.classList.add('is-hidden');
+}
+
+function showBtnLoadMore() {
+    loadMoreBtn.classList.remove('is-hidden');
+}
+
+function onLoadMore() {
+  if (fetchItems) {
+      fetchItems();     
+    }
+}
+      
+function makeCard(photos) {
+  const markup = photos.hits.map(({ tags, likes, webformatURL, comments, downloads, views, largeImageURL }) => {
         return `<a href="${largeImageURL}" class="photo-card">
             <img class="img" src="${webformatURL}" alt="${tags}" width="310" height="207" loading="lazy" />
             <div class="info">
@@ -101,26 +116,4 @@ function renderPhoto(photos) {
     }).join("");
 
     return galleryConteiner.insertAdjacentHTML("beforeend", markup);
-          
-    
-}
-
-function clearGalleryConteiner() {
-    galleryConteiner.innerHTML = '';
-}
-
-function hideBtnLoadMore() {
-    loadMoreBtn.classList.add('is-hidden');
-}
-
-function showBtnLoadMore() {
-    loadMoreBtn.classList.remove('is-hidden');
-}
-
-function onLoadMore() {
-  if (fetchItems) {
-      fetchItems();
-          
-    }
-     
 }
